70aaa241c25f073b1821dd038335041de7085f4f
Himanshu2401 <htiwari2401@gmail.com>
2021-06-04 12:38:01 +0530

    Second Commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser')
+const geolib = require('geolib');
+
+const { getDistance } = geolib;
 
 app.use( bodyParser.json() ); 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +20,16 @@ const dbClient = new MongoClient(
     }
 );
 
+const getUserDistance = ({
+    userLat,
+    userLong,
+    truckLat,
+    truckLong
+}) => getDistance(
+    { latitude: parseFloat(userLat), longitude: parseFloat(userLong) },
+    { latitude: truckLat, longitude: truckLong }
+);
+
 app.get('/', async (req, res) => {
     const data = await dbClient.db(
         'Solu_db'
@@ -28,21 +41,54 @@ app.get('/', async (req, res) => {
     res.json({ message: data });
 });
 
-app.post('/lat', async (req, res) => {
+app.get('/lat', async (req, res) => {
     console.log(req.body)
     const data = await dbClient.db(
         'Solu_db'
     ).collection(
         'Table'
     ).find({
-        Latitude: req.body.Latitude,
-        Longitude: req.body.Longitude
+        Latitude: { $ne: req.body.Latitude },
+        Longitude: { $ne: req.body.Longitude }
     }).toArray();
     res.json({ message: data });
+});
+
+app.get('/nearest', async (req, res) => {
+    const {
+        body: {
+            Latitude: userLat,
+            Longitude: userLong
+        }
+    } = req;
+    const data = await dbClient.db(
+        'Solu_db'
+    ).collection(
+        'Table'
+    ).find().toArray();
+
+    const distances = await Promise.all(
+        data.map((ele) => {
+            const diff = getUserDistance({
+                userLat,
+                userLong,
+                truckLat: ele.Latitude,
+                truckLong: ele.Longitude
+            });
+            return { ...ele, distance: diff }
+        })
+    );
+
+    const trucks = distances.sort(
+        (a, b) => a - b
+    ).filter(
+        (ele) => ele.distance != 0
+    );
+    res.json(trucks);
 });
 
 async function main() {
     await dbClient.connect();
     app.listen(port, () => console.log(`App listening on port ${port}`))
 }
-main();
+main()
